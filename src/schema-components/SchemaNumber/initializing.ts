56444f9c0649b2b071bf9ca2c558bedf158471fa
mah9 <mah9@ziroom.com>
2022-11-11 18:12:28 +0800

    edit

---
+++ src/schema-components/SchemaNumber/initializing.ts
@@ -0,0 +1,18 @@
+/*
+ * @Author: M.H
+ * @Date: 2022-11-11 17:58:59
+ * @LastEditors: M.H
+ * @LastEditTime: 2022-11-11 18:11:43
+ * @Description: 请填写简介
+ */
+
+export default {
+  label: '数字',
+  type: 'number',
+  property: '',
+  value: 0,
+  options: {
+    min: 0,
+    max: 50,
+  },
+};
