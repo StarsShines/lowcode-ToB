f1097893a7dc4901530cc3733312e0319f838665
mah9 <mah9@ziroom.com>
2022-11-11 18:14:17 +0800

    edit

--- src/utils/registerCompenents.ts
+++ src/utils/registerCompenents.ts
@@ -2,18 +2,16 @@
  * @Author: M.H
  * @Date: 2022-11-07 17:08:29
  * @LastEditors: M.H
- * @LastEditTime: 2022-11-11 17:24:43
+ * @LastEditTime: 2022-11-11 18:14:04
  * @Description: 组件引入
  */
 import McContainer from '@material/McContainer/index.vue';
-import McTab from '@material/McTab/index.vue';
 import McTitle from '@material/McTitle/index.vue';
 
 import SchemaNumber from '@schema/SchemaNumber/index.vue';
 
 export const GLOBAL_MATERIAL_COMPONENTS: any = {
   McContainer,
-  McTab,
   McTitle,
 };
 
